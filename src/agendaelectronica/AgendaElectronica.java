bd993604547338ce7c6c828ba49828a1f133b835
Martin Lopez <martinlopez45630@gmail.com>
2024-06-29 13:46:45 -0300

    Error en Vista al llamar  txtIndice

---
+++ src/agendaelectronica/AgendaElectronica.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package agendaelectronica;
+
+import Vista.Vista;
+
+
+/**
+ *
+ * @author Usuario
+ */
+public class AgendaElectronica {
+
+    
+    public static void main(String[] args) {
+        Vista vista = new Vista();
+        
+        
+    }
+    
+}
